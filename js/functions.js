a1d135ab29fc387b333a83ab9f6a9a9d89ec45b5
Martin Pizango <eduardo.martin688@gmail.com>
2021-12-29 00:49:29 -0500

    Agregando cambios

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
 
-    // Transition effect for navbar and back-to-top icon
     $(window).scroll(function () {
-        // checks if window is scrolled more than 500px, adds/removes solid class
+
         if ($(this).scrollTop() > 550) {
             $('.header').addClass('solid');
             $('.back-to-top').addClass('visible');
@@ -20,16 +19,6 @@ function alerta(){
     alert("hola")
 }
 
-/* let loginForm = document.querySelector('.login-form');
-
-document.querySelector('#login-btn').onclick = () =>{
-   loginForm.classList.add('active');
-} */
-
-/* document.querySelector('#close-login-form').onclick = () =>{
-   loginForm.classList.remove('active');
-} */
-
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .nav');
 
@@ -39,7 +28,6 @@ menu.onclick = () =>{
 }
 
 window.onscroll = () =>{
-   loginForm.classList.remove('active');
    menu.classList.remove('fa-times');
    navbar.classList.remove('active');
 
